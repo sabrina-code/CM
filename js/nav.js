649ac90abb59f25c9b3b7e302faf65d8b9884d27
Sabrina Fan <sfan@aqueous.com>
2024-10-11 07:49:32 -0700

    feat: modifies nav action function to keep line steady while mouse is moving

--- js/nav.js
+++ js/nav.js
@@ -1,59 +1,60 @@
-let activeLink = null; // Variable to hold the currently active link
+// Variable to hold the currently active link
+let activeLink = null;
 
+// Convert rem to pixels before shrinking the underline. We need to shrink 1.7rem on each side to fit the line to the width of the text
+const shrinkAmount =
+  1.7 * parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+// Feed nav content dynamically
 const createNavBar = (cities) => {
   const cityList = document.querySelector('.nav__list');
 
   cities.forEach((city, index) => {
     const li = document.createElement('li');
     const a = document.createElement('a');
-
-    // Add class to the elements
     li.classList.add('nav__item');
     a.classList.add('nav__link');
-
     a.href = `#${city.section}`;
     a.textContent = city.label;
-
     li.appendChild(a);
     cityList.appendChild(li);
 
     // Default the first item as active
-    if (index === 0) {
-      setActiveLink(a);
-    }
+    if (index === 0) setActiveLink(a);
   });
 
-  // Call linkListeners after creating the links to listen to the mouse event
   linkListeners();
 };
 
-const setActiveLink = (link) => {
+// shrink underline to fit in the width of the text
+const updateUnderline = (link) => {
   const nav = document.querySelector('.nav__list');
-  if (activeLink) {
-    activeLink.classList.remove('active'); // Remove active class from the previous link
-  }
-  link.classList.add('active');
-  activeLink = link; // Update the active link
-
   const { offsetWidth, offsetLeft } = link;
-  const adjustedWidth = Math.max(offsetWidth - 50, 0);
+  const adjustedWidth = Math.max(offsetWidth - shrinkAmount * 2, 0);
   nav.style.setProperty('--underline-width', `${adjustedWidth}px`);
-  nav.style.setProperty('--underline-offset-x', `${offsetLeft + 25}px`);
+  nav.style.setProperty(
+    '--underline-offset-x',
+    `${offsetLeft + shrinkAmount}px`
+  );
 };
 
+// Remove active class from the previous link before setting a new active link
+const setActiveLink = (link) => {
+  if (activeLink) activeLink.classList.remove('active');
+  link.classList.add('active');
+
+  activeLink = link;
+  updateUnderline(link);
+};
+
+// mouse action event listener
 const linkListeners = () => {
   const nav = document.querySelector('.nav__list');
   const links = document.querySelectorAll('.nav__link');
 
   nav.addEventListener('mouseover', (event) => {
     const link = event.target.closest('.nav__link');
-    if (link && !link.classList.contains('active')) {
-      const { offsetWidth, offsetLeft } = link;
-
-      const adjustedWidth = Math.max(offsetWidth - 50, 0);
-      nav.style.setProperty('--underline-width', `${adjustedWidth}px`);
-      nav.style.setProperty('--underline-offset-x', `${offsetLeft + 25}px`);
-    }
+    if (link && !link.classList.contains('active')) updateUnderline(link);
   });
 
   links.forEach((element) => {
@@ -61,13 +62,7 @@ const linkListeners = () => {
 
     // revert to previous active item if condition met
     element.addEventListener('mouseleave', () => {
-      if (activeLink) {
-        // Only revert to active link if it exists
-        const { offsetWidth, offsetLeft } = activeLink;
-        const adjustedWidth = Math.max(offsetWidth - 50, 0);
-        nav.style.setProperty('--underline-width', `${adjustedWidth}px`);
-        nav.style.setProperty('--underline-offset-x', `${offsetLeft + 25}px`);
-      }
+      if (activeLink) updateUnderline(activeLink);
     });
   });
 };
